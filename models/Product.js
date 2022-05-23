f35241c896a250486b246e5b8339105301d56ea7
Nadimfr <nadim.farah@hotmail.com>
2022-05-23 09:18:03 +0300

    Nadim

--- models/Product.js
+++ models/Product.js
@@ -1,30 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        require: true,
-        min: 2,
-        max: 40,
-        unique: true
-    },
-    image: {
-        type: String,
-        required: true,
-        max: 75,
-        unique: false,
-    },
-    price: {
-        type: Number,
-        required: true,
-        unique: false,
-    },
-    type: {
-        type: String,
-        required: true,
-        unique: false,
-    }
-    
+  name: {
+    type: String,
+    require: true,
+    min: 2,
+    max: 40,
+    unique: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    max: 75,
+    unique: false,
+  },
+  price: {
+    type: Number,
+    required: true,
+    unique: false,
+  },
+  category: {
+    type: String,
+    required: true,
+    unique: false,
+  },
 });
 
 module.exports = mongoose.model("Product", productSchema);
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -6,5 +6,6 @@ router.get("/:id", productController.getProduct);
 router.put("/:id", productController.updateProduct);
 router.get("/", productController.getProducts);
 router.post("/", productController.createProduct);
+// router.get("/type", productController.getProductsbyType);
 
 module.exports = router;
